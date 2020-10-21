363298afb1ec6a2ee8e2673e66b20e81d4d452f4
DarSpace <45310234+DarSpace@users.noreply.github.com>
2020-10-21 20:18:26 +0200

    ProductModel

---
+++ zad1/zad1/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using zad1.Models;
+
+
+namespace zad1.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult ProductModel()
+        {
+            var product = new List<ProductModel>
+            {
+              new ProductModel { NameProducts = "Marchew", Descriptions = "opis 1 ", Price = 10 },
+              new ProductModel { NameProducts = "Ananas",Descriptions = "opis 2 ", Price = 13   },
+              new ProductModel { NameProducts= "Pizza", Descriptions = "opis 3 ",  Price = 15   },
+              new ProductModel { NameProducts = "Lody", Descriptions = "opis 4 ",   Price = 18  },
+
+            };
+            return View(product);
+
+        }
+
+        
+        
+
+
+
+    }
+}
---
+++ zad1/zad1/Models/ProductModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace zad1.Models
+{
+    public class ProductModel
+    {
+        public string NameProducts { get; set; }
+        public string Descriptions { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
